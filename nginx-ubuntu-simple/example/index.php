9c3bcb9dffa3168bd8216423db20322c9800c478
Fintan Ryan <fintanr@gmail.com>
2015-02-19 18:56:55 +0000

    Remove IP reference in PHP example

--- nginx-ubuntu-simple/example/index.php
+++ nginx-ubuntu-simple/example/index.php
@@ -1,7 +1,6 @@
 {
     "message" : "Hello Weave - nginx example",
     "hostname" : <?php echo gethostname() ?>",
-    "ip" : <?php echo gethostbyname(gethostname()) ?>,
     "date" : "<?php echo date('Y-m-d H:i:s') ?>"
 }
 
