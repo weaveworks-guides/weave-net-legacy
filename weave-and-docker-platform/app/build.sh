cd0315d309d8c131a9634d76185b2d541501aea2
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-06-18 11:00:35 +0100

    Only build on our VMs, to be safe

--- weave-and-docker-platform/app/build.sh
+++ weave-and-docker-platform/app/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-for m in $(docker-machine ls -q); do
+for m in $(docker-machine ls -q | grep weave-); do
   echo "Building on '${m}':"
   docker $(docker-machine config ${m}) build -t app_web .
 done
