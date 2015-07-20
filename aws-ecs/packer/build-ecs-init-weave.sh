a9a221dca312a60d8db5342d38c7ccf1e8b21279
Alfonso Acosta <fons@weave.works>
2015-07-20 19:54:21 +0000

    [aws-ecs] Fix typo

--- aws-ecs/packer/build-ecs-init-weave.sh
+++ aws-ecs/packer/build-ecs-init-weave.sh
@@ -2,7 +2,7 @@
 
 # Build dev branch of ecs-init
 docker build -t ecs-init-weave -f ecs-init-weave.dockerfile  .
-echo -n 'Saving ecs-init-1.2.1-2.el7.centos.x86_64.rpm .. '
+echo -n 'Saving ecs-init-1.3.0-1.el7.centos.x86_64.rpm .. '
 docker run ecs-init-weave:latest cat /root/rpmbuild/RPMS/x86_64/ecs-init-1.3.0-1.el7.centos.x86_64.rpm > ecs-init-1.3.0-1.el7.centos.x86_64.rpm
 echo Done
 
