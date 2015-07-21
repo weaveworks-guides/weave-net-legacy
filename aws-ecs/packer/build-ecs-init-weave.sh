cb4aa3f49b7e1770ebbd98011f237e970e867e7e
Alfonso Acosta <fons@weave.works>
2015-07-21 08:29:52 +0000

    [aws-ecs] Separate build scripts from files to upload

--- aws-ecs/packer/build-ecs-init-weave.sh
+++ aws-ecs/packer/build-ecs-init-weave.sh
@@ -3,11 +3,11 @@
 # Build dev branch of ecs-init
 docker build -t ecs-init-weave -f ecs-init-weave.dockerfile  .
 echo -n 'Saving ecs-init-1.3.0-1.el7.centos.x86_64.rpm .. '
-docker run ecs-init-weave:latest cat /root/rpmbuild/RPMS/x86_64/ecs-init-1.3.0-1.el7.centos.x86_64.rpm > ecs-init-1.3.0-1.el7.centos.x86_64.rpm
+docker run ecs-init-weave:latest cat /root/rpmbuild/RPMS/x86_64/ecs-init-1.3.0-1.el7.centos.x86_64.rpm > to-upload/ecs-init-1.3.0-1.el7.centos.x86_64.rpm
 echo Done
 
 # Build master branch of Weave
 docker run -v /var/run/docker.sock:/var/run/docker.sock weaveworks/weave-build https://github.com/weaveworks/weave.git
 echo -n 'Saving weave.tar .. '
-docker save weaveworks/weave:latest weaveworks/weaveexec:latest > weave.tar
+docker save weaveworks/weave:latest weaveworks/weaveexec:latest > to-upload/weave.tar
 echo Done
