3ab21380902bbe25ac291a6c744e61921bf130d0
Alfonso Acosta <fons@weave.works>
2015-09-20 21:48:49 +0000

    Add timeout to cleanup script as well

--- aws-ecs/test.sh
+++ aws-ecs/test.sh
@@ -4,25 +4,42 @@ COUNTER=0
 # 10 minutes
 TIMEOUT=600
 
+run_with_timeout() {
+    TOUT=$1
+    shift
+    # Run timeout in the background so that we can kill it from the shell
+    timeout $TIMEOUT $@ &
+    TIMEOUTPID=$!
+    trap 'kill -INT -$TIMEOUTPID; exit 1' INT
+    wait $TIMEOUTPID
+    RET=$?
+    # restore trap
+    trap - INT
+    return $RET
+}
+
+
 while true; do
     echo
-    echo "CLEARING UP at $(date)"
-    ./cleanup.sh
+    echo "CLEANING UP at $(date)"
+    # Sometimes cleanup.sh gets stuck, run it again
+    while true; do
+	if ! run_with_timeout $TIMEOUT ./cleanup.sh; then
+	    echo
+	    echo "CLEAN UP TIMEOUT, CLEANING UP AGAIN at $(date)"
+	else
+	    break
+	fi
+    done
     sleep 60
     
     echo
     echo "STARTING UP ($COUNTER times so far) at $(date)"
-    # Run timeout in the background so that we can kill it from the shell
-    timeout $TIMEOUT ./setup.sh &
-    TIMEOUTPID=$!
-    trap 'kill -INT -$TIMEOUTPID; exit 1' INT
-    if ! wait $TIMEOUTPID ; then
+    if ! run_with_timeout $TIMEOUT ./setup.sh ; then
 	echo FAILURE
 	exit 1
     fi
     let COUNTER++
-    # restore trap
-    trap - INT
     sleep 15
 done
     
