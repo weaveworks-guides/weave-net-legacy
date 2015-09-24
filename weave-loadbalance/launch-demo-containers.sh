db90acb70e588f173ec872d2f83f8d7a4f03147a
Anita Buehrle <anitab@summatrix.com>
2015-09-24 16:11:48 -0500

    updated for weave 1.1

--- weave-loadbalance/launch-demo-containers.sh
+++ weave-loadbalance/launch-demo-containers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-vagrant ssh weave-gs-01 -c "eval \$(weave proxy-env); docker run -d -h lb.weave.local fintanr/myip-scratch"
-vagrant ssh weave-gs-01 -c "eval \$(weave proxy-env); docker run -d -h lb.weave.local fintanr/myip-scratch"
-vagrant ssh weave-gs-01 -c "eval \$(weave proxy-env); docker run -d -h lb.weave.local fintanr/myip-scratch"
-vagrant ssh weave-gs-02 -c "eval \$(weave proxy-env); docker run -d -h lb.weave.local fintanr/myip-scratch"
-vagrant ssh weave-gs-02 -c "eval \$(weave proxy-env); docker run -d -h lb.weave.local fintanr/myip-scratch"
-vagrant ssh weave-gs-02 -c "eval \$(weave proxy-env); docker run -d -h lb.weave.local fintanr/myip-scratch"
+vagrant ssh weave-gs-01 -c "eval \$(weave env); docker run -d -h loadbalance.weave.local weaveworks/myip-scratch"
+vagrant ssh weave-gs-01 -c "eval \$(weave env); docker run -d -h loadbalance.weave.local weaveworks/myip-scratch"
+vagrant ssh weave-gs-01 -c "eval \$(weave env); docker run -d -h loadbalance.weave.local weaveworks/myip-scratch"
+vagrant ssh weave-gs-02 -c "eval \$(weave env); docker run -d -h loadbalance.weave.local weaveworks/myip-scratch"
+vagrant ssh weave-gs-02 -c "eval \$(weave env); docker run -d -h loadbalance.weave.local weaveworks/myip-scratch"
+vagrant ssh weave-gs-02 -c "eval \$(weave env); docker run -d -h loadbalance.weave.local weaveworks/myip-scratch"
--- weave-loadbalance/setup-weave.sh
+++ weave-loadbalance/setup-weave.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-vagrant ssh weave-gs-01 -c "weave launch -iprange 10.2.0.1/16"
-vagrant ssh weave-gs-02 -c "weave launch -iprange 10.2.0.1/16 172.17.8.101"
-vagrant ssh weave-gs-01 -c "weave launch-dns; weave launch-proxy"
-vagrant ssh weave-gs-02 -c "weave launch-dns; weave launch-proxy"
+vagrant ssh weave-gs-01 -c "weave launch --ipalloc-range 10.2.0.1/16"
+vagrant ssh weave-gs-02 -c "weave launch --ipalloc-range 10.2.0.1/16 172.17.8.101"
