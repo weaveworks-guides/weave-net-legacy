bfd975f67e022f931358f8cda02af14ef424470e
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-10-08 15:54:31 +0100

    Use re-tagged images

--- microservices-seneca-ubuntu-simple/launch-senca-demo.sh
+++ microservices-seneca-ubuntu-simple/launch-senca-demo.sh
@@ -7,6 +7,6 @@ vagrant ssh weave-gs-02 -c "weave launch 172.17.8.101"
 
 echo "Launching our Node.js application"
 
-vagrant ssh weave-gs-01 -c 'docker $(weave config) run -d --name=offer-ms fintanr/seneca_offer' >/dev/null 2>&1
-vagrant ssh weave-gs-02 -c 'docker $(weave config) run -d --name=user-ms fintanr/seneca_user' >/dev/null 2>&1
-vagrant ssh weave-gs-01 -c 'docker $(weave config) run -d --name=web -p 80:80 fintanr/seneca_webapp' >/dev/null 2>&1
+vagrant ssh weave-gs-01 -c 'docker $(weave config) run -d --name=offer-ms weaveworks/seneca_offer' >/dev/null 2>&1
+vagrant ssh weave-gs-02 -c 'docker $(weave config) run -d --name=user-ms weaveworks/seneca_user' >/dev/null 2>&1
+vagrant ssh weave-gs-01 -c 'docker $(weave config) run -d --name=web -p 80:80 weaveworks/seneca_webapp' >/dev/null 2>&1
