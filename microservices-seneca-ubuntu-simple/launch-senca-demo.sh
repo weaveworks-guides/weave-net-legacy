2573e1c09cb7e3c3ee85285900eb454e744d75e5
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-10-08 12:34:49 +0100

    Make `vagrant up ; ./launch-senca-demo.sh ;` work

--- microservices-seneca-ubuntu-simple/launch-senca-demo.sh
+++ microservices-seneca-ubuntu-simple/launch-senca-demo.sh
@@ -1,14 +1,12 @@
-#!/bin/bash 
+#!/bin/bash -x
 
-echo "Launching Weave and WeaveDNS"
+echo "Launching Weave"
 
-vagrant ssh weave-gs-01 -c "sudo weave launch" 
-vagrant ssh weave-gs-01 -c "sudo weave launch-dns 10.2.1.1/24" 
-vagrant ssh weave-gs-02 -c "sudo weave launch 172.17.8.101" 
-vagrant ssh weave-gs-02 -c "sudo weave launch-dns 10.2.1.2/24"
+vagrant ssh weave-gs-01 -c "weave launch 172.17.8.102"
+vagrant ssh weave-gs-02 -c "weave launch 172.17.8.101"
 
-echo "Launching our node.js application"
+echo "Launching our Node.js application"
 
-vagrant ssh weave-gs-01 -c "sudo weave run --with-dns 10.3.1.1/24 -h offer-ms.weave.local fintanr/seneca_offer" >/dev/null 2>&1
-vagrant ssh weave-gs-02 -c "sudo weave run --with-dns 10.3.1.2/24 -h user-ms.weave.local fintanr/seneca_user" >/dev/null 2>&1
-vagrant ssh weave-gs-01 -c "sudo weave run --with-dns 10.3.1.3/24 -p 80:80 -h web.weave.local fintanr/seneca_webapp" >/dev/null 2>&1
+vagrant ssh weave-gs-01 -c 'docker $(weave config) run -d --name=offer-ms fintanr/seneca_offer' >/dev/null 2>&1
+vagrant ssh weave-gs-02 -c 'docker $(weave config) run -d --name=user-ms fintanr/seneca_user' >/dev/null 2>&1
+vagrant ssh weave-gs-01 -c 'docker $(weave config) run -d --name=web -p 80:80 fintanr/seneca_webapp' >/dev/null 2>&1
