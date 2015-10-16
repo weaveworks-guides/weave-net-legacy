b0b4a0f2ce19521592ec11c8f31b18d9c0d9fd91
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-10-16 13:23:42 +0100

    Separate router and proxy units, so all logs go into the journal

--- mesos-marathon/centos/install_weave.sh
+++ mesos-marathon/centos/install_weave.sh
@@ -3,4 +3,4 @@
 cd $(dirname $0)
 
 install -o root -g root -m 0755 weave /usr/bin/
-install -o root -g root -m 0644 weave.target weave.service /etc/systemd/system/
+install -o root -g root -m 0644 weave.target weave.service weaveproxy.service /etc/systemd/system/
--- mesos-marathon/centos/setup_and_launch_weave.sh
+++ mesos-marathon/centos/setup_and_launch_weave.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 printf 'WEAVE_PEERS="%s"\n' "$*" > /etc/weave.env
-systemctl -q --no-block start weave.service
+systemctl -q --no-block start weaveproxy weave.service
