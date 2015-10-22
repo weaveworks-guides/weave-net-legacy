10376a9f920b9be81342b31bf42fe21941016bc4
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-10-22 10:25:12 -0700

    Close #119

--- mesos-marathon/centos/install_mesosphere_packages.sh
+++ mesos-marathon/centos/install_mesosphere_packages.sh
@@ -3,4 +3,4 @@
 cd $(dirname $0)
 
 rpm -i http://repos.mesosphere.io/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
-yum -q -y install mesos-0.23.0 marathon-0.9.1 mesosphere-zookeeper-3.4.6
+yum -q -y install mesos-0.25.0 marathon-0.11.1 mesosphere-zookeeper-3.4.6
