276fba3ff67e7f263277e83ec91151247ea1884d
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-10-23 13:23:10 -0700

    Fix and clean-up `2-weave-launch.sh`

--- weave-and-docker-platform/scripts/2-weave-launch.sh
+++ weave-and-docker-platform/scripts/2-weave-launch.sh
@@ -1,18 +1,12 @@
 #!/bin/bash -e
 
-## find_tls_args="cat /proc/\$(pgrep /usr/local/bin/docker)/cmdline | tr '\0' '\n' | grep ^--tls | tr '\n' ' '"
-
 for i in $(seq 3) ; do
   ## This environment variable is respected by Weave script,
   ## hence it needs to be exported
-  ## export DOCKER_CLIENT_ARGS="$(docker-machine config weave-${i})"
-
-  ## tlsargs=$(docker-machine ssh "weave-${i}" "${find_tls_args}")
+  export DOCKER_CLIENT_ARGS="$(docker-machine config weave-${i})"
 
   ## We are going to use IPAM, hence we supply estimated cluster size
   ./weave launch --init-peer-count 3
-  ## set the proxy for weave
-  weave $(weave env)
 
   ## Let's connect-up the Weave cluster by telling
   ## each of the nodes about the head node (weave-1)
@@ -20,5 +14,3 @@ for i in $(seq 3) ; do
     ./weave connect $(docker-machine ip 'weave-1')
   fi
 done
-
-
