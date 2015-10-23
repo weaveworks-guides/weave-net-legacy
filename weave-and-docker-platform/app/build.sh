9b45a04ca0d72e191a8be739f50054a619dd8196
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-10-23 16:04:39 -0700

    Speed-up the build process and avoid out of disk space errors

--- weave-and-docker-platform/app/build.sh
+++ weave-and-docker-platform/app/build.sh
@@ -1,6 +1,10 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
-for m in $(docker-machine ls -q | grep weave-); do
-  echo "Building on '${m}':"
-  docker $(docker-machine config ${m}) build -t app_web .
-done
+echo "Building on 'weave-1':"
+docker $(docker-machine config 'weave-1') build -t app_web .
+echo "Copying from 'weave-1' to 'weave-2' and 'weave-1'..."
+docker $(docker-machine config 'weave-1') save app_web \
+  | tee \
+    >(docker $(docker-machine config 'weave-2') load) \
+    >(docker $(docker-machine config 'weave-3') load) \
+  | cat > /dev/null
