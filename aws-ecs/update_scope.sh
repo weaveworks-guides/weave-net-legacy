b62eec0002ffb6720e4f6c56c2e24e4a11384c0f
Alfonso Acosta <fons@weave.works>
2015-10-27 11:59:32 +0000

    Improve Scope updating

--- aws-ecs/update_scope.sh
+++ aws-ecs/update_scope.sh
@@ -2,15 +2,10 @@
 
 set -euo pipefail
 
-#Hack to overcome bindfs permission problems in my machine
-cp `dirname $0`/weave-ecs-demo-key.pem /tmp/
-chmod 0600 /tmp/weave-ecs-demo-key.pem
-KEY=/tmp/weave-ecs-demo-key.pem
-
 ssh_to_instance() {
     HOST=$1
     shift
-    ssh -o "StrictHostKeyChecking no" -i $KEY ec2-user@$HOST $@
+    ssh -o "StrictHostKeyChecking no" -i `dirname $0`/weave-ecs-demo-key.pem ec2-user@$HOST $@
 }
 
 SCOPE_IMAGE_FILE=$(mktemp /tmp/scope-update-image-tar-XXXX)
@@ -28,13 +23,18 @@ for HOST in $INSTANCE_HOSTNAMES; do
     echo Updating $HOST;
     echo Current scope image:
     ssh_to_instance $HOST docker images weaveworks/scope
+    echo -n 'Stopping Scope and removing images .. '
+    ssh_to_instance $HOST -t sudo stop scope > /dev/null || true
+    ssh_to_instance $HOST docker rm -f weavescope > /dev/null || true
+    ssh_to_instance $HOST docker rmi '$(docker images -q weaveworks/scope)' > /dev/null || true
     echo -n 'Updating Scope image .. '
     ssh_to_instance $HOST docker load < $SCOPE_IMAGE_FILE
     echo done
-    echo New Scope image:
+    echo 'New Scope image:'
     ssh_to_instance $HOST docker images weaveworks/scope
     echo Updating scope script:
     ssh_to_instance $HOST -t sudo curl https://raw.githubusercontent.com/weaveworks/scope/master/scope -o /usr/local/bin/scope \; sudo chmod +x /usr/local/bin/scope
-    echo Restarting Scope:
-    ssh_to_instance $HOST -t sudo stop scope \; sudo start scope
+    echo -n 'Restarting Scope .. '
+    ssh_to_instance $HOST -t sudo start scope
+    echo done
 done
