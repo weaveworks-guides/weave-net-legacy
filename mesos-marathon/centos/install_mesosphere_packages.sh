567486c190e489372bd7c7ad45ca2c3237edff9e
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-07-11 12:09:08 +0100

    Bump Mesosphere RPM package versions

--- mesos-marathon/centos/install_mesosphere_packages.sh
+++ mesos-marathon/centos/install_mesosphere_packages.sh
@@ -2,5 +2,5 @@
 
 cd $(dirname $0)
 
-rpm -i http://repos.mesosphere.io/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
-yum -q -y install mesos-0.27.0 marathon-0.15.0 mesosphere-zookeeper-3.4.6
+rpm -i http://repos.mesosphere.io/el/7/noarch/RPMS/mesosphere-el-repo-7-3.noarch.rpm
+yum -q -y install mesos-0.28.2 marathon-1.1.1 mesosphere-zookeeper-3.4.6
