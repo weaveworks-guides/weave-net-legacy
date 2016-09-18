afd18472a7f5acdf6c21a46bb916aba60b74b9b1
Bryan Boreham <bjboreham@gmail.com>
2016-09-18 10:18:53 -0700

    Download latest weave script instead of embedded (old) one

--- mesos-marathon/centos/install_weave.sh
+++ mesos-marathon/centos/install_weave.sh
@@ -2,5 +2,6 @@
 
 cd $(dirname $0)
 
+curl -L -s -o weave https://git.io/weave
 install -o root -g root -m 0755 weave /usr/bin/
 install -o root -g root -m 0644 weave.target weave.service weaveproxy.service /etc/systemd/system/
